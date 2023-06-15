119836f6789360adab7b8aa961b3cee4a844bbd7
CorruptedClan <105439556+CorruptedClan@users.noreply.github.com>
2023-06-15 11:59:22 +0300

    Update DoubleDownPremium.lua

--- DoubleDownPremium.lua
+++ DoubleDownPremium.lua
@@ -1,6 +1,1 @@
---Username 
-if game.Players.LocalPlayer.Username== {"Username1", "Username2", "Username3"} then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/CorruptedClan/TheObsfucatedCode/main/DoubleDownUI.lua.txt"))() 
-else 
-game.Players.LocalPlayer:Kick("Not Whitelisted") 
-end
+loadstring(game:HttpGet("https://raw.githubusercontent.com/CorruptedClan/TheObsfucatedCode/main/DoubleDownUI.lua.txt"))()
